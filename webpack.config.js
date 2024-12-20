dcb4356178e4129ce5d89e73b120c06c66369dbc
Sarahi Mireles <sarahimireles@users.noreply.github.com>
2024-12-20 18:21:10 -0500

    BM-1 Initial commit: set up react application and dependencies (#9)
    * BM-1 Initial commit: set up react application and dependencies
    
    * BM-1 indentation fix
    
    ---------
    
    Co-authored-by: Sarahi Mireles <edith.mireles@baresdev.com>

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.tsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+  devServer: {
+    static: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 3000,
+  },
+  devtool: 'source-map',
+};
