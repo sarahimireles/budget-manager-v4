d48d069f07262d0d4ed957f34bb6c086a9855ca1
Ruben Cardenas <rubencg88@gmail.com>
2024-12-21 08:24:56 -0500

    Added rules to eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
+import globals from "globals"
+import pluginJs from "@eslint/js"
+import tseslint from "typescript-eslint"
+import pluginReact from "eslint-plugin-react"
 
 
 /** @type {import('eslint').Linter.Config[]} */
@@ -13,5 +13,24 @@ export default [
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  {
+    settings: {
+      react: {
+        version: "detect" // Automatically detects React version from your dependencies
+      }
+    },
+    rules: {
+      "react/react-in-jsx-scope": "off",
+      "react/jsx-filename-extension": ["warn", { "extensions": [".jsx", ".tsx"] }],
+      "import/extensions": "off",
+      "no-unused-vars": "warn",
+      "@typescript-eslint/no-unused-vars": ["warn"],
+      "react/prop-types": "off",
+      "react/function-component-definition": "off",
+      "linebreak-style": 0,
+      "semi": ["error", "never"],
+      "quotes": ["error", "double"]
+    }
+  },
   {ignores: ["node_modules", "build", "dist", ".vscode", ".git", ".eslintrc.*", "*.config.js"]},
-];
+]
