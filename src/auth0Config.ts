8f2f338002ee7d015e8f29345dead9e5db0adf36
Ruben Cardenas <rubencg88@gmail.com>
2025-11-15 11:23:42 -0600

    BM-64 Added auth0 login and removed firebase

---
+++ src/auth0Config.ts
@@ -0,0 +1,10 @@
+export const auth0Config = {
+  domain: process.env.REACT_APP_AUTH0_DOMAIN || "",
+  clientId: process.env.REACT_APP_AUTH0_CLIENT_ID || "",
+  authorizationParams: {
+    redirect_uri: window.location.origin,
+    ui_locales: "es-MX",
+  },
+  cacheLocation: "localstorage" as const,
+  useRefreshTokens: true,
+}
